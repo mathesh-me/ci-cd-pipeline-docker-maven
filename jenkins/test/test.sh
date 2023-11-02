9fa848d117337f644fa3a89771f06561767ee7fa
mathesh-me <matheshme679@gmail.com>
2023-11-02 23:08:24 +0530

    jenkins folder is added

---
+++ jenkins/test/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "#################################"
+echo "######### Building Jar ##########"
+echo "#################################"
+
+WORKSPACE=/home/ubuntu/jenkins_home/workspace/maven-project
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven "$@"
